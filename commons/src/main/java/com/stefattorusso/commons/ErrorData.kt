bf8b13f472764257505930f2abd0df969c202a64
Stefano <stefano.fattorusso@berepublic.es>
2019-04-16 10:33:43 +0200

    Update kotlin version

--- commons/src/main/java/com/stefattorusso/commons/ErrorData.kt
+++
@@ -1,7 +0,0 @@
-package com.stefattorusso.commons
-
-class ErrorData (message: String = "", errorCode: String = "", throwable: Throwable? = null){
-    var message: String = message
-    var errorCode: String = errorCode
-    var throwable: Throwable? = throwable
-}
